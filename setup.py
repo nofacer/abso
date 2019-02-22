0d75a0e4e3f9e6fd883619d912def53ec1a52a88
nofacer <nofacer@outlook.com>
2019-02-22 20:00:15 +0800

    init

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ from setuptools import setup
 setup(name='abso',
       version='0.1',
       description='Set root path to a specific position.',
-      url='http://github')
+      url='https://github.com/nofacer/abso.git',
+      author="Dustni",
+      author_email='dustni@qq.com',
+      license='MIT',
+      packages=['abso'],
+      zip_safe=False
+      )
